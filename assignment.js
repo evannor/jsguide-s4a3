a862f549173e9b636c4a5b350c0a545f79973c6c
evannor <erica.vannorman@gmail.com>
2020-12-08 11:26:14 -0800

    Loop through array of numbers

--- assignment.js
+++ assignment.js
@@ -3,3 +3,17 @@ const randomNumber = Math.random(); // produces random number between 0 (includi
 if (randomNumber > 0.7) {
   alert("Random number is greater than 0.7: " + randomNumber);
 }
+
+const newArray = [21, 63, 69, 42, 96];
+const evenArray = [];
+// Output even numbers
+for (i=0; i< newArray.length; i++) {
+  if(newArray[i] % 2 === 0) {
+    evenArray.push(newArray[i]);
+  }
+}
+console.log(evenArray);
+
+for (const number of newArray){
+  console.log(number);
+}
