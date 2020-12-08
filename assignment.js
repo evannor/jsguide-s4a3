96e1e4e4b7e653b454e61a2ec1c964bf4ce14ce9
evannor <erica.vannorman@gmail.com>
2020-12-08 11:10:13 -0800

    Add alert

--- assignment.js
+++ assignment.js
@@ -1,1 +1,5 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+if (randomNumber > 0.7) {
+  alert("Random number is greater than 0.7: " + randomNumber);
+}
