27e818728d27991bbbb194942b3c5da8a71b45ea
evannor <erica.vannorman@gmail.com>
2020-12-08 11:40:26 -0800

    New random #, control statements

--- assignment.js
+++ assignment.js
@@ -1,8 +1,8 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
 
-if (randomNumber > 0.7) {
-  alert("Random number is greater than 0.7: " + randomNumber);
-}
+// if (randomNumber > 0.7) {
+//   alert("Random number is greater than 0.7: " + randomNumber);
+// }
 
 const newArray = [21, 63, 69, 42, 96];
 const evenArray = [];
@@ -22,4 +22,15 @@ for (const number of newArray){
 // Print out items of array backwards
 for (i = newArray.length; i >= 0; i--) {
   console.log(newArray[i]);
+}
+
+// New random number and alerts
+const newRandom = Math.random();
+
+if (newRandom > 0.7 && randomNumber > 0.7) {
+  alert("Both numbers are greater than 0.7");
+} else if (newRandom < 0.2 || randomNumber < 0.2) {
+  alert(randomNumber + " " + newRandom + " are the numbers");
+} else {
+  alert("Numbers didn't pass either conditions");
 }
